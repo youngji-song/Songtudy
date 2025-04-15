3d15a65c7a8198dbf14967b043f13a077395363a
kngyns <99dbstj99@naver.com>
2025-04-15 20:50:11 +0900

    [boj]_15651_N과M(3)

---
+++ [week3]Apr14_Apr20/yoonseo/bj15651.py
@@ -0,0 +1,13 @@
+def dfs():
+    if len(ans) == M:
+        print(' '.join(map(str, ans)))
+        return
+    
+    for i in range(1, N+1):
+        ans.append(i)
+        dfs()
+        ans.pop()
+
+N, M = map(int, input().split())
+ans = []
+dfs()
