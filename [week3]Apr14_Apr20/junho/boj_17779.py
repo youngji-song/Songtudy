c9efdb9386926acb0070d311302efe88f57fc30f
pjh <pjh199999@gmail.com>
2025-04-18 00:53:39 +0900

    [BOJ] 게리맨더링2/17779/G3/5시간

---
+++ [week3]Apr14_Apr20/junho/boj_17779.py
@@ -0,0 +1,42 @@
+def cal(si,sj,d1,d2):
+    v = [[0]*N for _ in range(N)]
+    alst = [0]*5
+
+    v[si][sj] = 1
+    j1=j2=sj
+
+    for di in range(1,d1+d2+1):
+        if di <= d1: j1-=1
+        else:        j1+=1
+
+        if di<=d2:  j2+=1
+        else:       j2-=1
+        v[si+di][j1:j2+1]=[1]*(j2-j1+1)
+
+    for i in range(N):
+        for j in range(N):
+            if v[i][j]==1: continue
+            if i<si+d1 and j<=sj: alst[0]+=arr[i][j]
+            if i<=si+d2 and sj<j: alst[1]+=arr[i][j]
+            if si+d1<=i and j<sj-d1+d2: alst[2]+=arr[i][j]
+            if si+d2<i and sj-d1+d2<=j: alst[3]+=arr[i][j]
+    alst[4]=tot-sum(alst)
+    # print(alst)
+    return max(alst)-min(alst)
+
+N = int(input())
+arr = [list(map(int,input().split())) for _ in range(N)]
+
+tot = sum(map(sum,arr))
+
+ans = 100*N*N
+
+for si in range(N-2):
+    for sj in range(1,N-1):
+        for d1 in range(1,N):
+            if 0<=si+d1<N and 0<=sj-d1<N:
+                for d2 in range(1,N):
+                    if 0<=si+d1+d2<N and sj+d2<N:
+                        ans = min(ans,cal(si,sj,d1,d2))
+
+print(ans)
