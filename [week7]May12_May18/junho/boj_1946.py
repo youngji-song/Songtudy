b37c1a0b7f7c2520c1eb8257b9ca19dafcb03194
pjh <pjh199999@gmail.com>
2025-05-12 22:58:51 +0900

    [BOJ] 신입사원/1946/S1/2시간 30분

--- [week7]May12_May18/junho/boj_1946.py
+++ [week7]May12_May18/junho/boj_1946.py
@@ -2,31 +2,12 @@ T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     scores = [list(map(int, input().split())) for _ in range(N)]
-    ans = 0
-    order_sorted = sorted(scores)
-    order_sorted2 = sorted(scores, key=lambda x: x[1])
-    lst_1 = []
-    lst_2 = []
-
-    for i in range(N):
-        if order_sorted[0][1] > order_sorted[i][1]:
-            lst_1.append(order_sorted[i])
-        if order_sorted2[0][0] > order_sorted2[i][0]:
-            lst_2.append(order_sorted2[i])
-
-    # print(lst_1)
-    # print(lst_2)
-    # 교집합 구하기
-    set_1 = set(tuple(item) for item in lst_1)
-    set_2 = set(tuple(item) for item in lst_2)
-
-    # 교집합
-    intersection = set_1 & set_2
-
-    # 다시 리스트로 변환
-    common_items = [list(item) for item in intersection]
-
-    print(len(common_items)+2)
-
-
+    scores_order = sorted(scores)
+    idx_high = 0
+    ans = 1
+    for i in range(1,N):
+        if scores_order[i][1] < scores_order[idx_high][1]:
+            idx_high = i
+            ans += 1
     
+    print(ans)
